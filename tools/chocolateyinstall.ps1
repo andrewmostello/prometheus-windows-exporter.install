d3138241cef46b7fa62e410a31bb3c90ef77f63d
Andrew Mostello <amostello@expertek.com>
2020-09-08 11:34:55 -0400

    Update to 0.14.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'prometheus-windows-exporter.install'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.13.0/windows_exporter-0.13.0-386.msi'
-$url64      = 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.13.0/windows_exporter-0.13.0-amd64.msi'
+$url        = 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.14.0/windows_exporter-0.14.0-386.msi'
+$url64      = 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.14.0/windows_exporter-0.14.0-amd64.msi'
 
 $pp = Get-PackageParameters
 
@@ -48,9 +48,9 @@ $packageArgs = @{
 
   softwareName  = 'windows_exporter*'
 
-  checksum      = '6F5BF52A3DB999F4B4C8B9E96FD2329F0CFAE0C547D39AD645E167145E0EA62C'
+  checksum      = 'E33CAE7232528E88311F4E8E727B3C02F145575723D8A3AA36EA32C50C3DA02D'
   checksumType  = 'sha256'
-  checksum64    = '6E5C67A8648823A80931A915590731D33D435F8A10A9EB8A7DCE087AE473ED9A'
+  checksum64    = 'AAAD64650A4BA69178BDCC4873A219580E31FCAADAFA3C912FB31FA59F7F8E2C'
   checksumType64= 'sha256'
 
   silentArgs    = $silentArgs
